105bdfd331ec438eda6fa6405d37c79f1ad6a1fc
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-06-08 12:37:39 -0500

    reading messages only

---
+++ src/javascripts/helpers/data/messagesData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const firebaseUrl = apiKeys.firebaseConfig.databaseURL;
+
+const getMessages = () => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/messages.json`)
+    .then((results) => {
+      const messageResults = results.data;
+      const messages = [];
+      Object.keys(messageResults).forEach((message) => {
+        messageResults[message].id = message;
+        messages.push(messageResults[message]);
+      });
+      console.error(messages);
+      resolve(messages);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getMessages };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
 import auth from './components/auth/auth';
-
 import apiKeys from './helpers/apiKeys.json';
+import messageData from './helpers/data/messagesData';
 
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   auth.authBuilder();
+  messageData.getMessages();
 };
 
 init();
