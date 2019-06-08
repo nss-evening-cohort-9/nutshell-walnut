6542148d68f606665579814efa26fb5308c678e9
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-08 10:42:38 -0500

    auth done

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import util from '../../helpers/util';
+
+
+import googleImage from './googlebtn.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const authBuilder = () => {
+  let domString = '<h2 class="log-title">Log In</h2><button id ="google-auth">';
+  domString += `<img class="button" src=${googleImage}/>`;
+  domString += '</div>';
+  util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
+};
+
+export default { authBuilder };
