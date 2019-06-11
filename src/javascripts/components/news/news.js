722d9e8970ac2e5d68e9f29491f5f04cc9719554
Mark Okafor <okaformark@gmail.com>
2019-06-10 19:49:17 -0500

    events by uid prints to dom

--- src/javascripts/components/news/news.js
+++ src/javascripts/components/news/news.js
@@ -2,11 +2,15 @@ import util from '../../helpers/util';
 import newsData from '../../helpers/data/newsData';
 
 const newsDomStringBulder = (news) => {
-  console.error(news);
-  let domString = '<div class = "container d-flex hide">';
+  let domString = '<div class = "container container-news d-flex hide">';
   news.forEach((newsItem) => {
-    domString += '<div class = "card">';
-    domString += `<div class="card-header">${newsItem.title}</div>`;
+    domString += '<div class = "box card">';
+    domString += '<div class = "content">';
+    domString += '<h2>01</h2>';
+    domString += `<div class="card-title">${newsItem.title}</div>`;
+    domString += `<p class="">${newsItem.synopsis}</p>`;
+    domString += `<a href="${newsItem.url}">Read more</a>`;
+    domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('news', domString);
